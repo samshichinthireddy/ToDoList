5a09f5a9967c8212bb5576aab2d842f9c723156f
Samshi Chinthireddy <samshi.chinthireddy@gmail.com>
2025-07-25 13:31:05 -0700

    Created 2 pages.

---
+++ NewToDoView.swift
@@ -0,0 +1,34 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack{
+            Text("Task title:")
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                    .cornerRadius(15)
+                      .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
