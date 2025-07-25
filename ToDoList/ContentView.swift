cd30e99a3953aa9aa93f97837803b48c39e88d10
Samshi Chinthireddy <samshi.chinthireddy@gmail.com>
2025-07-25 13:38:30 -0700

    Added smooth animations and connected the two files.

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack {
@@ -16,7 +17,9 @@ struct ContentView: View {
                     .fontWeight(.bold)
                 Spacer()
                 Button{
-                    
+                    withAnimation {
+                        showNewTask = true
+                    }
                 } label: {
                     Text("+")
                         .font(.title)
@@ -27,6 +30,9 @@ struct ContentView: View {
             .padding()
             Spacer()
             
+        }
+        if showNewTask {
+            NewToDoView()
         }
     }
 }
